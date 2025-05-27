2d85396ece86d4212d7fed6dc0023cdfdf5f2720
Josip Vulic <jovulic@gmail.com>
2025-05-27 10:25:21 -0400

    ftr(nix): update nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A ZFS-based network storage layer over iSCSI with CSI integration.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     microvm = {
       url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
