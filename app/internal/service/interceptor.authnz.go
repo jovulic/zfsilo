5e745aeb59b98e2145cb1fb30fa4838e3c8dc96f
Josip Vulic <jovulic@gmail.com>
2025-08-09 21:35:22 -0400

    ftr(app): add authnz interceptor

---
+++ app/internal/service/interceptor.authnz.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"context"
+	"crypto/subtle"
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"connectrpc.com/connect"
+	slogctx "github.com/veqryn/slog-context"
+)
+
+type authnzContextKey struct{}
+
+type authnzContextValue struct {
+	identity string
+	token    string
+}
+
+func newAuthnzInterceptor(
+	packs []struct {
+		identity string
+		token    string
+	},
+) connect.UnaryInterceptorFunc {
+	return connect.UnaryInterceptorFunc(func(next connect.UnaryFunc) connect.UnaryFunc {
+		return func(
+			ctx context.Context,
+			req connect.AnyRequest,
+		) (connect.AnyResponse, error) {
+			authHeader := req.Header().Get("Authorization")
+			if authHeader == "" {
+				return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("no authorization header provided"))
+			}
+
+			// We currently only support bearer tokens, so that is what we assert here.
+			if !strings.HasPrefix(authHeader, "Bearer") {
+				return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("authorization header is not a bearer token"))
+			}
+
+			// We intentionally iterate over a list and perform constant time compares.
+			token := strings.TrimPrefix(authHeader, "Bearer")
+			identity := func() string {
+				for _, pack := range packs {
+					if subtle.ConstantTimeCompare([]byte(token), []byte(pack.token)) == 1 {
+						return pack.identity
+					}
+				}
+				return ""
+			}()
+			if identity == "" {
+				return nil, connect.NewError(connect.CodeUnauthenticated, fmt.Errorf("failed authorization"))
+			}
+
+			// Embed the authnz value into the context and extend the logger with
+			// identity information if the logger exists.
+			ctx = context.WithValue(ctx, authnzContextKey{}, authnzContextValue{
+				identity: identity,
+				token:    token,
+			})
+			ctx = slogctx.With(ctx, slog.String("identity", identity))
+
+			return next(ctx, req)
+		}
+	})
+}
