a5dc15eda6cd783a7f09da58f36318cb3b21661a
Josip Vulic <jovulic@gmail.com>
2025-08-09 21:02:46 -0400

    ftr(app): add log interceptor
    The interceptor does the following.
    - Attaches a logger to the request context.
    - Decorates the logger with procedure, request id, etc.
    - Logs the request body if it can.
    - Logs the response time.

---
+++ app/internal/service/interceptor.log.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"connectrpc.com/connect"
+	ulid "github.com/oklog/ulid/v2"
+	slogctx "github.com/veqryn/slog-context"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+func newLogInterceptor(log *slog.Logger) connect.UnaryInterceptorFunc {
+	return connect.UnaryInterceptorFunc(func(next connect.UnaryFunc) connect.UnaryFunc {
+		return func(
+			ctx context.Context,
+			req connect.AnyRequest,
+		) (connect.AnyResponse, error) {
+			ctx = slogctx.NewCtx(ctx, log)
+
+			procedure := req.Spec().Procedure
+			ctx = slogctx.With(ctx, slog.String("procedure", procedure))
+
+			requestId := ulid.Make()
+			ctx = slogctx.With(ctx, slog.String("requestId", requestId.String()))
+
+			slogctx.Info(ctx, "request received")
+
+			// We also log the request body, marshaled as JSON, if we are able to.
+			{
+				protoMsg, ok := req.Any().(proto.Message)
+				if !ok {
+					slogctx.Error(ctx, "request body for is not a proto.Message")
+				} else {
+					jsonBytes, err := protojson.Marshal(protoMsg)
+					if err != nil {
+						slogctx.Error(ctx, "failed to marshal request body to json", slogctx.Err(err), slog.Any("body", protoMsg))
+					} else {
+						slogctx.Info(ctx, "parsed body", slog.String("body", string(jsonBytes)))
+					}
+				}
+			}
+
+			startTime := time.Now()
+			res, err := next(ctx, req)
+			endTime := time.Now()
+
+			responseTime := endTime.Sub(startTime)
+			slogctx.Info(ctx, "request completed", slog.Duration("responseTime", responseTime))
+
+			return res, err
+		}
+	})
+}
