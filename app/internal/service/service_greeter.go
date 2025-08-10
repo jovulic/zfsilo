a56f4208142ea71be14e07b717d543a44f9edd0a
Josip Vulic <jovulic@gmail.com>
2025-08-10 12:11:11 -0400

    upd(app): remove protovalite within greeter as it is handled in interceptor

--- app/internal/service/service_greeter.go
+++ app/internal/service/service_greeter.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"buf.build/go/protovalidate"
 	"connectrpc.com/connect"
 	zfsilov1 "github.com/jovulic/zfsilo/api/gen/go/zfsilo/v1"
 	"github.com/jovulic/zfsilo/api/gen/go/zfsilo/v1/zfsilov1connect"
@@ -19,12 +18,7 @@ func NewGreeterService() *GreeterService {
 }
 
 func (g *GreeterService) SayHello(ctx context.Context, request *connect.Request[zfsilov1.SayHelloRequest]) (*connect.Response[zfsilov1.SayHelloResponse], error) {
-	if err := protovalidate.Validate(request.Msg); err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("invalid request: %w", err))
-	}
-
 	name := request.Msg.GetName()
-
 	return connect.NewResponse(
 		&zfsilov1.SayHelloResponse{
 			Message: fmt.Sprintf("Hello %s!", name),
