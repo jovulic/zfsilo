ae8bb6a5f425fa282489aea2db178c89a43aafe6
Josip Vulic <jovulic@gmail.com>
2026-02-02 18:34:33 -0500

    add(app): introduce producer and list of consumers in command

--- app/internal/command/wire.go
+++ app/internal/command/wire.go
@@ -9,24 +9,45 @@ import (
 )
 
 var WireSet = wire.NewSet(
-	WireExecutor,
+	WireProduceTarget,
+	WireConsumeTarget,
 )
 
-func WireExecutor(conf config.Config) (command.Executor, error) {
-	switch conf.Command.Mode {
+func buildExecutor(target config.ConfigCommandTarget) (command.Executor, error) {
+	switch target.Mode {
 	case "local":
-		return command.NewLocalExector(command.LocalExecutorConfig{
-			RunAsRoot: conf.Command.RunAsRoot,
+		return command.NewLocalExecutor(command.LocalExecutorConfig{
+			RunAsRoot: target.RunAsRoot,
 		}), nil
 	case "remote":
 		return command.NewRemoteExecutor(command.RemoteExecutorConfig{
-			RunAsRoot: conf.Command.RunAsRoot,
-			Address:   conf.Command.Remote.Address,
-			Port:      conf.Command.Remote.Port,
-			Username:  conf.Command.Remote.Username,
-			Password:  conf.Command.Remote.Password,
+			RunAsRoot: target.RunAsRoot,
+			Address:   target.Remote.Address,
+			Port:      target.Remote.Port,
+			Username:  target.Remote.Username,
+			Password:  target.Remote.Password,
 		}), nil
 	default:
-		return nil, fmt.Errorf("unknown command mode: %s", conf.Command.Mode)
+		return nil, fmt.Errorf("unknown command mode: %s", target.Mode)
 	}
+}
+
+type ProduceExecutor command.Executor
+
+func WireProduceTarget(conf config.Config) (ProduceExecutor, error) {
+	return buildExecutor(conf.Command.ProduceTarget.ConfigCommandTarget)
+}
+
+type ConsumeExecutorMap map[string]command.Executor
+
+func WireConsumeTarget(conf config.Config) (ConsumeExecutorMap, error) {
+	rets := make(map[string]command.Executor)
+	for i, target := range conf.Command.ConsumeTargets {
+		ret, err := buildExecutor(target.ConfigCommandTarget)
+		if err != nil {
+			return nil, fmt.Errorf("failed to process consume target %d: %w", i, err)
+		}
+		rets[target.IQN] = ret
+	}
+	return rets, nil
 }
